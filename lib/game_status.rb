b3bb78f4e30029a4babb9feb3eb7e57536194dd0
Ryan Davey <ryancdavey@icloud.com>
2018-06-27 22:10:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combination|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
