80e191d8abc41dc2990cf4d5d14e674f6093db0d
Ryan Davey <ryancdavey@icloud.com>
2018-06-27 22:08:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,42 +16,54 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-winner = []
-empty_board = board.all? {|x| x == " "}
-WIN_COMBINATIONS.each do |win_combination|
-    if empty_board || full?(board)
-      return nil
-    elsif win_combination.all? { |value| board[value] =="X" } || win_combination.all? { |value| board[value] =="O" }
-      winner = win_combination
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-  end
-  return winner
+  }
+  return false
 end
 
 def full?(board)
-  !board.any? { |x| x == " " }
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board) || !won?(board)
+  else
     return false
   end
 end
 
-def over?(array3)
-  if full?(array3) || !(won?(array3)) || draw?(array3)
-    puts "GAME OVER"
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
     return true
-  else 
-    puts "it ain't over till it's over baby!"
-    return false 
-  end 
-end  
+  else
+    return false
+  end
+end
 
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
